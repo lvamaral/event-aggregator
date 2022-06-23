6ed385d3037dade6068a1657458e014410765cc9
Lucas Amaral <lamaral@Lucass-MacBook-Pro.local>
2022-06-23 12:14:00 -0700

    Add docker-compose & questdb

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:16.15-alpine3.14
-WORKDIR /app
-COPY . .
-RUN npm install
-EXPOSE 3000
-ENTRYPOINT ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const customerRouter = require('./src/routes/customers');
+const customerRouter = require('./app/routes/customers');
 
 // Configure App
 app.use(express.json());
 
 // Register Routes
-app.use('v1/customers', customerRouter);
-/// Basic catch-all for bad routes
+app.use('/v1/customers', customerRouter);
+// Basic catch-all for bad routes
 app.use((req, res) => {
-  res.status(404).send('Route not found.');
+  res.status(404).send('Route not found!');
 });
 
 // Launch on specified port
